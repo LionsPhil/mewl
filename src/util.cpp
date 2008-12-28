c7bc013b73c50df5e72e1a731f352dc533ad0fd9
LionsPhil <LionsPhil@users.noreply.github.com>
2008-12-28 03:06:33 +0000

    Add missing files from last commit.
    git-svn-id: svn+ssh://private/mule@1465 51cd4cf5-f2fb-0310-83f2-91b3fd76ea51

---
+++ src/util.cpp
@@ -0,0 +1,26 @@
+#include "platform.hpp"
+#include "util.hpp"
+
+static const double ROOT_2 = 1.41421356237309504880168872420969807856967187537694807317667973799;
+
+static double phi(double x, double stddev) {
+	return 0.5 * (1 + platform_erf(x / (stddev * ROOT_2)));
+}
+
+int random_normal(int min, int max, double stddev) {
+	double r = platform_random();
+	for(int bound = min; bound < max; bound++) {
+		if(r < phi(bound + 0.5, stddev))
+			{ return bound; }
+	}
+	return max;
+}
+
+int random_uniform(int min, int max) {
+	int r;
+	do {
+		r = (int) (platform_random() * ((max + 1) - min));
+	} while(r > max); // 1/RAND_MAX chance of this happening
+	return r;
+}
+
---
+++ src/util.hpp
@@ -0,0 +1,16 @@
+#ifndef UTIL_HPP_
+#define UTIL_HPP_
+
+/** Generate a random integer in the inclusive range given with probability
+ *  given by the normal distribution with mean zero and standard deviation
+ *  provided. In future, this could perhaps be implemented using C++ TR1's
+ *  normal_distribution malarky in <random>, but at the time of writing that
+ *  appears to be somewhat on the new side of stable and well-documented. */
+int random_normal(int min, int max, double stddev);
+
+/** Generate a random number in the inclusive range given with a uniform
+ *  probability distribution. Again, C++ TR1 is a future possibility here. */
+int random_uniform(int min, int max);
+
+#endif
+
